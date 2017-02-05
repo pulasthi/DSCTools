4f2054228f0ea695ef7b3b4ac14b31700cfab0da
pulasthi <pulasthi911@gmail.com>
2017-02-05 18:19:42 -0500

    adding mds scripts

---
+++ Fusheng_Wang_image_bio/scripts/mds/README.md
@@ -0,0 +1,1 @@
+These scripts and config files are for the image TCGA-55-6972-01Z-00-DX1.0b441ad0-c30f-4f63-849a-36c98d6e2d3b
---
+++ Fusheng_Wang_image_bio/scripts/mds/run.all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+nodes=28
+name="$nodes"n
+nodefile=nodes.txt
+
+./run.generic.sh 2 12 $name samplerun 2 g $nodes $nodefile 

75c88e22b1c414a26e4d0d8038419501d7b0cb6d
pulasthi <pulasthi911@gmail.com>
2017-02-05 18:20:22 -0500

    adding mds scripts

--- Fusheng_Wang_image_bio/scripts/run.all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# nodes.txt file keeps the ip's of the nodes. 1 per each line
-nodes=28
-name="$nodes"n
-nodefile=nodes.txt
-
-./run.generic.sh 1 24 $name samplerun 1 g $nodes $nodefile 
